25afebaa437ec2b1aaba57980c4709bac9ccf88b
Kennedy Hooper <khooper5@charlotte.edu>
2025-11-15 21:30:03 -0500

    create authenticate, authRoutes, authorizeRoles

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { signUpHandler, logInHandler } from '../controllers/authController.js';
+import {validateUser} from '../middleware/userValidators.js';
+
+const router = express.Router();
+
+router.post('/signup', validateUser, signUpHandler);
+router.post('/login', loginLimiter, validateUser, logInHandler);
