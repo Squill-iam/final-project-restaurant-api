a294d5f2b179c8c046592bb9464dc8e264da08ea
Kennedy Hooper <khooper5@charlotte.edu>
2025-11-16 01:35:07 -0500

    created  userRoutes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import{
+    getAllUsersHandler, 
+    getCurrentUserHandler, 
+    updateCurrentUserHandler, 
+    deleteCurrentUserHandler, 
+    getUserReservationsHandler, 
+} from '../controllers/userController.js';
+
+import {authenticate} from '../middleware/authenticate.js';
+import {authorizeRoles} from '../middleware/authorizeRoles.js';
+import {validateUserUpdate} from '../middleware/userValidators.js';
+
+const router = express.Router();
+
+router.get('/', authenticate, authorizeRoles('ADMIN'), getAllUsersHandler);
+router.get('/id', authenticate, getCurrentUserHandler);
+router.put('/users/id', authenticate, validateUserUpdate, updateCurrentUserHandler);
+router.delete('/users/id', authenticate, deleteCurrentUserHandler);
+router.get('/users/id/reservations', authenticate, getUserReservationsHandler);
